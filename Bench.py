99d3dbf5514c776c262259b30fd10551e089762d
Flavio Junior <129971867+junin005@users.noreply.github.com>
2024-09-18 13:51:50 -0300

    Create Bench.py

---
+++ Bench.py
@@ -0,0 +1,8 @@
+nome = str(input('Qual seu nome? '))
+if nome == 'Flavio':
+    print('Que nome bonito')
+elif nome == 'Isadora' or nome == 'Laura':
+    print('Que nome lindo')
+else:
+    print('Olá! ')
+print(f'Tenha um bom dia {nome}!')  
